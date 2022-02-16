9b3eb2ac83ae5c481a3e57832be4ffe1a73a5792
ErickSchuenck <esfmokdeci@gmail.com>
2022-02-16 10:49:30 -0300

    agora criadas as funções que irão abrir a tela de quizz, alocadas numa div teste

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const quizz_URL = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+console.log(quizz_URL);
+
 function create_quizz(){
     console.log("função create-quizz sendo executada")
+}
+function hide_main_screen(){
+    console.log("função hide_main_screen sendo executada")
+    let main = document.querySelector('.main');
+    main.classList.add("hidden");
+}
+function display_quizz_screen(){
+    console.log("função display_quizz sendo executada")
+    let quizz_screen = document.querySelector('.quizz-screen');
+    quizz_screen.classList.remove("hidden");
 }
