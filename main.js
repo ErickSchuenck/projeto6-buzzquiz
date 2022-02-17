fa15363f3ee26004f315ed69512e51c3914ab4ec
ErickSchuenck <esfmokdeci@gmail.com>
2022-02-17 15:37:30 -0300

    agora funções em camel case

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
-const quizz_URL = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
-let victory_counter = 0;
+const quizzURL = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+let victoryCounter = 0;
 
 
-function create_quizz(){
-    console.log("função create-quizz sendo executada")
+function createQuizz(){
+    console.log("função create quizz sendo executada")
 }
-function hide_main_screen(){
-    console.log("função hide_main_screen sendo executada")
+function hideMainScreen(){
+    console.log("função hideMainScreen sendo executada")
     let main = document.querySelector('.main');
     main.classList.add("hidden");
 }
-function display_quizz_screen(){
-    console.log("função display_quizz sendo executada")
-    let quizz_screen = document.querySelector('.quizz-screen');
-    quizz_screen.classList.remove("hidden");
+function displayQuizzScreen(){
+    console.log("função displayQuizz sendo executada")
+    let quizzScreen = document.querySelector('.quizz-screen');
+    quizzScreen.classList.remove("hidden");
 }
-function reveal_cards_value(e){
+function revealCardsValue(e){
     console.log ("função reveal cards value sendo executada");
     let cards = e.parentNode.parentNode.querySelectorAll('.wrong');
         for (let i = 0; i< cards.length; i++){
         cards[i].classList.add("whitish")
     }
-    let wrong_text = e.parentNode.parentNode.querySelectorAll('.wrong-text');
-    for (let i = 0; i< wrong_text.length; i++){
-        wrong_text[i].classList.add("reddish")
+    let wrongText = e.parentNode.parentNode.querySelectorAll('.wrong-text');
+    for (let i = 0; i< wrongText.length; i++){
+        wrongText[i].classList.add("reddish")
     }
-    let right_text = e.parentNode.parentNode.querySelectorAll('.right-text');
-    for (let i = 0; i< right_text.length; i++){
-        right_text[i].classList.add("greenish")
+    let rightText = e.parentNode.parentNode.querySelectorAll('.right-text');
+    for (let i = 0; i< rightText.length; i++){
+        rightText[i].classList.add("greenish")
     }
 }
-function disable_cards(e){
+function disableCards(e){
     console.log ("função disable cards sendo executada")
     let cards = e.parentNode.parentNode.querySelectorAll('.card');
     for (let i = 0; i< cards.length; i++){
         cards[i].onclick.disabled = true
     } 
-    setTimeout(scroll_into_next_question, 2000)
+    setTimeout(scrollIntoNextQuestion, 2000)
 }
-function add_victory_counter(){
+function addVictoryCounter(){
     console.log ("função add victory counter sendo executada");
-    victory_counter = victory_counter + 1;
-    console.log(victory_counter);
+    victoryCounter = victoryCounter + 1;
+    console.log(victoryCounter);
 }
-function scroll_into_next_question(){
+function scrollIntoNextQuestion(){
     console.log ("função scroll into next question sendo executada");
 }
-function restart_quizz(){
+function restartQuizz(){
     console.log('restart quizz')
 }
